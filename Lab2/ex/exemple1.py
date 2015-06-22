e840262c2333e8fc5c380cdb48a06b8350c2cc29
lighta <lightaisme@gmail.com>
2015-06-22 19:59:12 -0400

    Init commit for lab2

---
+++ Lab2/ex/exemple1.py
@@ -0,0 +1,22 @@
+﻿# -*- coding: utf-8 -*-
+
+import nltk
+from nltk import *
+
+debug=1 # 0 or 1
+with open ("exemple1.cfg", "r") as myfile:
+    grammaireText=myfile.read()
+
+grammar = grammar.FeatureGrammar.fromstring(grammaireText)
+parser = nltk.ChartParser(grammar)
+tokens = "Jean tua Marie blsnl fgdfd Marie".split()
+
+
+parser = parse.FeatureEarleyChartParser(grammar)
+trees = parser.parse(tokens)
+for tree in trees:
+    if debug==1:
+        print(tree)
+        nltk.draw.tree.draw_trees(tree)
+    print(tree.label()['SEM'])
+
---
+++ Lab2/lab.py
@@ -0,0 +1,38 @@
+﻿# -*- coding: utf-8 -*-
+
+import nltk
+from nltk import *
+
+debug=0 # 0 or 1
+
+sentences= []
+#with open ("Einstein.txt", "r") as data:
+#	sentences=data.readlines()
+sentences.append('Jean tua Marie')
+
+with open ("lab.cfg", "r") as myfile:
+	grammaireText=myfile.read()
+	
+
+grammar = grammar.FeatureGrammar.fromstring(grammaireText)
+parser = nltk.ChartParser(grammar)
+parser = parse.FeatureEarleyChartParser(grammar)
+
+facts = []
+s = 0
+for sen in sentences:
+	s+=1
+	print ("s="+str(s)+" sen=>"+sen)
+	tokens = sen.split()
+	trees = parser.parse(tokens)
+	for tree in trees:
+		if debug==1:
+			print(tree)
+			nltk.draw.tree.draw_trees(tree)
+		facts.append(tree.label()['SEM'])
+		print(tree.label()['SEM'])
+
+#write output
+with open ("facts.clp", "w") as foutput:
+	for sen in facts:
+		foutput.write(str(sen))
