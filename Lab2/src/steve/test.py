8aa909524ec26d5f611e402b657542e7909aedc3
ac29620 <ac29620@gmail.com>
2015-06-24 19:58:51 -0400

    2 phrasew steve
    Le Suédois a des chiens comme animaux domestiques.
    La personne qui boit du thé est un Danois.

---
+++ Lab2/src/steve/test.py
@@ -0,0 +1,20 @@
+﻿# -*- coding: utf-8 -*-
+
+import nltk
+from nltk import *
+
+with open ("2_phrase_steve.cfg", "r") as myfile:
+    grammaireText=myfile.read()
+
+grammar = grammar.FeatureGrammar.fromstring(grammaireText)
+parser = nltk.ChartParser(grammar)
+#tokens = "Le Suédois a des chiens comme animaux domestiques".split()
+tokens = "La personne qui boit du thé est un Danois".split()
+
+
+parser = parse.FeatureEarleyChartParser(grammar)
+trees = parser.parse(tokens)
+for tree in trees:
+    print(tree)
+    nltk.draw.tree.draw_trees(tree)
+    print(tree.label()['SEM'])
