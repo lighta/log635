42bfdbae0361d822e5010a311dd3a152504164f3
lighta <lightaisme@gmail.com>
2015-07-02 10:00:08 -0400

    Add dico check for nom propre

--- Lab2/src/lab.py
+++ Lab2/src/lab.py
@@ -12,27 +12,51 @@ showDrawnTree = 0 # 0 or 1
 #pattern_obj = re.compile(pattern)
 #replacement_string="\\1" + '_' + "\\2"
 
+npword= []
+with codecs.open("dico_np.txt", "r", encoding="UTF-8") as f:
+	fileContent2 = f.readlines()
+	
+for lineIndex2 in range(len(fileContent2)):
+	currentLine2 = str(fileContent2[lineIndex2])
+	currentLine2 = re.sub("(\r|\n)",'',currentLine2) #replace \r \n , with ' '
+	m6=re.search("^#",str(currentLine2))
+	if m6 != None:
+		print("Line is a comment2; will not use.")
+	if len(currentLine2) <= 1:
+		print("Line is a comment; will not use.")
+	else:
+		#print("Dico_np => "+currentLine2)
+		npword.append(currentLine2)
+	
 sentences= []
 with codecs.open("Einstein.txt", "r", encoding="UTF-8") as f:
 	fileContent = f.readlines()
 
+print("npword="+str(npword))
 for lineIndex in range(len(fileContent)):
-	print("Checking line number " + str(lineIndex) + "...")
-	currentLine = str(fileContent[lineIndex]).replace('\r\n','')
+	#print("Checking line number " + str(lineIndex) + "...")
+	currentLine = str(fileContent[lineIndex]) #.replace('\r\n','')
+	currentLine = re.sub("(\r|\n)",'',currentLine) #replace \r \n , with ' '
 	currentLine = currentLine.replace('Pall Mall','Pall_Mall')
 	currentLine = currentLine.replace('Blue Master','Blue_Master')# mettre ici tous les replace qu'on a besoin
 	#currentLine = pattern_obj.sub(replacement_string, currentLine)
 	currentLine = currentLine.replace('\'',' ')
 	currentLine = currentLine.replace(',','')
-	print("Line is '" + currentLine + "'")
-	if (len(currentLine) > 0 and currentLine[0] == "#"):
+	#print("Line is '" + currentLine + "'")
+	if (len(currentLine) <= 1 or currentLine[0] == "#"):
 		print("Line is a comment; will not use.")
 	else:
 		splitSentence = currentLine.split('.')		#chaque point donne une phrase
 		for oneSentence in splitSentence:
 			if (len(oneSentence) > 5): # completely arbitrary minimum length
-				sentences.append(oneSentence[0].lower() + oneSentence[1:])
-				sentences.append(oneSentence[0].upper() + oneSentence[1:])
+				oneSentence = oneSentence.strip()	#trim both side
+				words = oneSentence.split(' ')
+				if(str(words[0]) in npword):
+					#print("Word in dico") #on ne change pas le case
+					sentences.append(oneSentence)
+				else:	# on met en minuscules
+					#print("word0="+words[0]) 
+					sentences.append(oneSentence[0].lower() + oneSentence[1:])
 
 print("")
 
@@ -99,8 +123,7 @@ for sen in facts:
 			s[m.end()-1] = ''
 			newsen = "".join(s)
 			sen = newsen
-			#print("match="+str(m))
-			print("\tnewsen(np)=>"+str(sen))
+			#print("\tnewsen(np)=>"+str(sen))
 			doublecheck=0
 
 		#move verbe instead coma
@@ -111,7 +134,6 @@ for sen in facts:
 				break;
 			m4=re.search(",",str(sen))
 			m2pos = m2.start()
-			
 			m6=re.search("(\w+)\((\w+|\s|\|)+,(\w+|\s|\|)+\)",str(sen))			# meme regex preceder par un mot
 			if m6 == None: #no more VP substitution to do
 				break;
@@ -126,6 +148,7 @@ for sen in facts:
 			s[m4pos] = '' #replace , by ''
 			s[len(str(tmp))-1] = ' ' #replace ) by ' '
 			
+			#newsen = tmp[:m2pos]+tmp[m2pos:m4pos]+" "+word+" "+tmp[m4pos:]
 			s.insert(m4pos,' ')
 			i=1
 			for wi in list(str(word)):
@@ -136,7 +159,7 @@ for sen in facts:
 			
 			#print("\tnewsen=>"+str(newsen))
 			sen = sen[:m6.start()]+newsen+sen[m6.end():] #remove word from sentence
-			print("\tnewsen(vp2)=>"+str(sen))
+			#print("\tnewsen(vp2)=>"+str(sen))
 			doublecheck=0
 		
 		#regle 3 we have a list
@@ -161,8 +184,8 @@ for sen in facts:
 			tmp = "|list"+tmp
 			#print("\ttmp2=>"+str(tmp))
 			
-			sen = sen[:m2.start()]+tmp+sen[m2.end():] #remove word from sentence
-			print("\tnewsen(list)=>"+str(sen))
+			sen = sen[:m2.start()]+tmp+sen[m2.end():]
+			#print("\tnewsen(list)=>"+str(sen))
 			doublecheck=0
 		else:
 			doublecheck=1
@@ -174,7 +197,7 @@ for sen in facts:
 		s[m3.end()-1] = ')'
 		newsen = "".join(s)
 		sen = newsen
-		print("\tnewsen(list2)=>"+str(sen))
+		#print("\tnewsen(list2)=>"+str(sen))
 	
 	sen = re.sub("( )+",' ',sen)
 	print("\tcleaned=>"+str(sen))
