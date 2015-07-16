d082e4419e114b7cfb2e9c26e08b3ddad1c21c4a
Yumi <Yumi@Yumi-PC>
2015-07-16 19:49:16 -0400

    Start of

--- Lab3/src/log635_lab3/main.java
+++ Lab3/src/log635_lab3/main.java
@@ -3,7 +3,7 @@ package log635_lab3;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world");
+		System.out.println("Hello world, hello lighta");
 	}
 
 }
