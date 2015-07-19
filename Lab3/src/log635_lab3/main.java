ef80cd52d44cb0722f83359dc79aad3e2e637b30
DenkouNova <fggallant@gmail.com>
2015-07-19 17:46:53 -0400

    Added a class to parse the CSV into vectors of doubles

--- Lab3/src/log635_lab3/main.java
+++ Lab3/src/log635_lab3/main.java
@@ -1,9 +1,44 @@
-package log635_lab3;
+
+//package log635_lab3;
+
+import java.util.*;
+
 
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world, hello lighta");
+		Lab3FileReader lb3fr = new Lab3FileReader("D:\\Download\\Firefox\\Donnees_sources2.csv");
+		
+		// Print headers
+		for (int i = 0; i < lb3fr.GetHeaders().size(); i++)
+		{
+			System.out.println(lb3fr.GetHeaders().elementAt(i));
+		}
+		
+		System.out.println("---");
+		
+		// Print first column
+		for (int i = 0; i < lb3fr.GetDataColumn(0).size(); i++)
+		{
+			System.out.println(lb3fr.GetDataColumn(0).elementAt(i));
+		}
+		
+		System.out.println("---");
+		
+		// Print second column, even numbers only
+		for (int i = 0; i < lb3fr.GetDataColumnEvenRowsOnly(1).size(); i++)
+		{
+			System.out.println(lb3fr.GetDataColumnEvenRowsOnly(1).elementAt(i));
+		}
+		
+		System.out.println("---");
+		
+		// Print third column, odd numbers only
+		for (int i = 0; i < lb3fr.GetDataColumnOddRowsOnly(2).size(); i++)
+		{
+			System.out.println(lb3fr.GetDataColumnOddRowsOnly(2).elementAt(i));
+		}
 	}
 
 }
+
