8fbe0df4796958f184a0e1a6d1d61892d00f3f27
lighta <lightaisme@gmail.com>
2015-07-24 21:58:49 -0400

    Fixed, stuff and improve test

--- Lab3/src/log635_lab3/perceptron.java
+++ Lab3/src/log635_lab3/perceptron.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.PipedReader;
 import java.io.PipedWriter;
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
 
 public class perceptron extends Thread {
 	boolean running;
@@ -22,9 +24,8 @@ public class perceptron extends Thread {
 		this.outPipe = outPipe;
 		this.isSigmoid = true;
 		
-		int i=0;
 		inputPipes = new PipedReader[inPipes.length];
-		while(inPipes.length < i++){ // Randomize every weights.
+		for(int i=0; i < inPipes.length; i++){ // Randomize every weights.
 			inputWeights[i] = Math.random();
 		}
 		connect(inPipes,outPipe);
@@ -44,13 +45,16 @@ public class perceptron extends Thread {
 	
 	public double calcOutput(double[] inputs)
 	{
+		System.out.println("Perceptron entering calc");
 		double sum = 0, out=0;
 		// Calc the perceptron value.
 		for(int i=0; i < inputs.length; i++)
 		{
 			sum += inputs[i] * inputWeights[i];
+			System.out.println("Perceptron in["+i+"]="+(inputs[i] * inputWeights[i]));
 		}
-		sum -= bias * biasWeight;
+		sum += bias * biasWeight;
+		System.out.println("Perceptron bias="+(bias * biasWeight)+" sum="+sum+" mode="+((isSigmoid)?"sigmoid":"seuil"));
 					
 		if(isSigmoid){
 			// Activation function in this case sigmoid.
@@ -59,6 +63,8 @@ public class perceptron extends Thread {
 		else {
 			out = (sum >= 0)?1:0; //1 or 0
 		}
+		
+		System.out.println("Perceptron calc done => out="+out);
 		return out;
 	}
 	
@@ -97,97 +103,140 @@ public class perceptron extends Thread {
 	private void disconnect(){
 		try 
 		{
-			int i = 0;
-			while(inputPipes.length < i++)
+			for(int i=0; i < this.inputPipes.length; i++)
 			{ 
 				inputPipes[i].close();
+				System.out.println("Perceptron["+i+"]:: inputs pipes closed.");
 			}
-			System.out.println("StatusFilter:: inputs pipes closed.");
+			System.out.println("Perceptron:: inputs pipes closed.");
 			
 			outPipe.close();
-			System.out.println("StatusFilter:: output pipe closed.");
+			System.out.println("Perceptron:: output pipe closed.");
 			
 		} 
 		catch (Exception Error) 
 		{
-			System.out.println("StatusFilter:: Error while closing pipes.");
+			System.out.println("Perceptron:: Error while closing pipes.");
 		} 
 	}
 	
 	
 	public void run() {		
 		try {
-			int i = 0;
+			int j = 0;
 			boolean closepipe[] = new boolean[inputPipes.length];
-			while(inputPipes.length < i++)	{  // For each inputPipes.
-				closepipe[i] = false;
-			}
-			System.out.println("Perceptron stared");
+			for(int i=0; i < this.inputPipes.length; i++)
+				closepipe[j] = false; // For each inputPipes.
+
+			System.out.println("Perceptron started");
 			
 			running = true;
 			while (running) 
 			{
 				double[] inputsValue = new double[inputPipes.length];
-				i = 0;
 				
 				//@TODO pe test avec dataoutputstream
-				while(inputPipes.length < i++) // For each inputPipes.
+				for(int i=0; i < this.inputPipes.length; i++)
 				{ 
 					running = false;
-					if(closepipe[i] == true) //skip it
+					if(closepipe[i] == true){ //skip it
 						continue;
+					}
 					running = true; //on a tjr au moins un pipe ouvert
 					
 					boolean EOL = false; // End Of Line.
+					StringBuilder lineOfText = new StringBuilder();
+					
 					while(EOL == false)
 					{
-						String lineOfText = null;
-						char[] ch = new char[1]; // Char array is required to turn since pipes read one char at a time.
 						int value; // The value read from the pipe.
 						value = inputPipes[i].read();
-						ch[0] = (char) value;
+						char ch = (char) value;
+						//System.out.println("Perceptron["+i+"] read="+value);
+						
 						if (value == -1) { // Pipe is closed.
 							closepipe[i] = true;
+							break; // EOL = true;
 						} else {
-							lineOfText += new String( ch);
+							lineOfText.append(ch);
 						}
 						if (value == '\n') {  // End of line	
-							inputsValue[i] = Double.parseDouble( lineOfText );
+							inputsValue[i] = Double.parseDouble( lineOfText.toString() );
 							EOL = true;
+							System.out.println("Perceptron["+i+"] received="+inputsValue[i]);
 						}
 					}
 				}
 				double res = calcOutput(inputsValue);
 				outPipe.write(res + "\n");
 				outPipe.flush();
-			} 
-			
+			} 	
 			System.out.println("Perceptron stopped");
 		} 
 		catch (Exception error) 
 		{
-			System.out.println("StatusFilter:: Interrupted.");
+			System.out.println("Perceptron:: Interrupted. error="+error);
+			error.printStackTrace();
 		} 
 		disconnect();
 	}
 	
+	private static List<Double> readSortie(PipedWriter sortie){
+		PipedReader entree = new PipedReader();
+		List<Double> valsortie = new ArrayList<>();
+		try {
+			boolean done=false;
+			entree.connect(sortie);
+			
+			while(done==false){
+				double val=0;
+				boolean EOL = false;
+				StringBuilder lineOfText = new StringBuilder();
+				
+				while(EOL == false){
+					int value; // The value read from the pipe.
+					value = entree.read();
+					
+					char ch = (char) value;
+					if (value == -1) { // Pipe is closed.
+						done=true;
+						break;
+					} else {
+						lineOfText.append(ch);
+					}
+					if (value == '\n') {  // End of line	
+						val = Double.parseDouble( lineOfText.toString() );
+						EOL = true;
+						//System.out.println("val="+val);
+						valsortie.add(val);
+					}
+				}
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return valsortie;
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("Perception main, quicktest");
 		PipedWriter[] inputPipes = new PipedWriter[2]; //x1 et x2
 		double inputPipesW[] = new double[2];
-		int i=0;
-		while(i< inputPipes.length){
+
+		for(int i=0; i < inputPipes.length; i++){
 			inputPipes[i] = new PipedWriter();
 			inputPipesW[i] = 1.0;
-			i++;
 		}
-		PipedWriter outPipe = new PipedWriter();
+		PipedWriter EToutPipe = new PipedWriter();
+		PipedWriter OUoutPipe = new PipedWriter();
 		//PipedWriter[] inpipe, double[] inputWeights, double bias, double biasWeight,PipedWriter outpipe, boolean isSigmoid
-		perceptron ETpercep;
+		perceptron ETpercep, OUpercep;
 		try {
-			ETpercep = new perceptron(inputPipes,inputPipesW,-0.5,1,outPipe,false);
+			ETpercep = new perceptron(inputPipes,inputPipesW,-1.5,1,EToutPipe,false);
+		//	OUpercep = new perceptron(inputPipes,inputPipesW,-0.5,1,OUoutPipe,false);
 			ETpercep.start();
+		//	OUpercep.start();
 		} catch (Exception e) {
 			System.exit(1);
 		}
@@ -202,37 +251,10 @@ public class perceptron extends Thread {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		List<Double> sETpercep = readSortie(EToutPipe);
+		System.out.println("Sortie ET perceptron sETpercep="+sETpercep);
 		
-		PipedReader outPipeEnd = new PipedReader();
-		try {
-			outPipeEnd.connect(outPipe);
-			boolean done=false; 
-			
-			String lineOfText = null;
-			
-			while(done==false){
-				double val=0;
-				boolean EOL = false;
-				while(EOL == false){
-					char[] ch = new char[1]; // Char array is required to turn since pipes read one char at a time.
-					int value; // The value read from the pipe.
-					value = outPipeEnd.read();
-					ch[0] = (char) value;
-					if (value == -1) { // Pipe is closed.
-						done=true;
-					} else {
-						lineOfText += new String( ch);
-					}
-					if (value == '\n') {  // End of line	
-						val = Double.parseDouble( lineOfText );
-						EOL = true;
-						System.out.println("val="+val);
-					}
-				}
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	//	List<Double> sOUpercep = readSortie(OUoutPipe);
+	//	System.out.println("Sortie OU perceptron sETpercep="+sOUpercep);	
 	}
 }
